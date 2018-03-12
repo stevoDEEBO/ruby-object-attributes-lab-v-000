c07ac5b1825689810c1c80601ea706ff9a20e8bb
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-03-12 21:02:37 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,11 @@
 class Person
 
-  def initialize
-    @name = name
-  end
-
   def name
     @name
   end
 
   def name=(new_name)
     @name = new_name
-  end
-
-  def initialize
-    @job = job
   end
 
   def job
